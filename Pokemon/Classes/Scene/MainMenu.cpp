4a2ad4f9c260f611bb378bcfb9b86e48238318f8
nguyentantai15t3 <taijoke97@gmail.com>
2019-07-10 10:20:41 +0700

    + add init into mainmenu scene

--- Pokemon/Classes/Scene/MainMenu.cpp
+++ Pokemon/Classes/Scene/MainMenu.cpp
@@ -28,5 +28,57 @@ bool MainMenu::init()
 	backGround->setScale(visibleSize.width / backGround->getContentSize().width, visibleSize.height / backGround->getContentSize().height);
 	addChild(backGround, -99);
 
+	auto buttonPlay = ResourceManager::GetInstance()->GetButtonById(6);
+	buttonPlay->removeFromParent();
+	buttonPlay->setScale(0.2f);
+	buttonPlay->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 9));
+	addChild(buttonPlay, -1);
+	buttonPlay->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type)
+	{
+		if (type == Widget::TouchEventType::ENDED)
+		{
+			// replay scene
+		}
+	});
+
+	auto buttonSetting = ResourceManager::GetInstance()->GetButtonById(8);
+	buttonSetting->removeFromParent();
+	buttonSetting->setScale(0.1f);
+	buttonSetting->setPosition(Vec2(visibleSize.width / 20, visibleSize.height / 10));
+	addChild(buttonSetting, -1);
+	buttonSetting->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type)
+	{
+		if (type == Widget::TouchEventType::ENDED)
+		{
+			// create setting
+		}
+	});
+	auto buttonAbout = ResourceManager::GetInstance()->GetButtonById(5);
+	buttonAbout->removeFromParent();
+	buttonAbout->setScale(0.25f);
+	buttonAbout->setPosition(Vec2(visibleSize.width * 19 / 20, visibleSize.height / 10));
+	addChild(buttonAbout, -1);
+	buttonAbout->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type)
+	{
+		if (type == Widget::TouchEventType::ENDED)
+		{
+			// create about
+		}
+	});
+
+
+	auto buttonExit = ResourceManager::GetInstance()->GetButtonById(9);
+	buttonExit->removeFromParent();
+	buttonExit->setScale(0.1f);
+	buttonExit->setPosition(Vec2(visibleSize.width * 19 / 20, visibleSize.height * 9 / 10));
+	addChild(buttonExit, -1);
+	buttonExit->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type)
+	{
+		if (type == Widget::TouchEventType::ENDED)
+		{
+			Director::getInstance()->end();
+			exit(0);
+		}
+	});
 	return true;
 }
