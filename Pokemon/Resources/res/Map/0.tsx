3fb08651ae20d4236e7849ccc72240bcb2c19f6e
ngokhuongduy <ngokhuongduy39@gmail.com>
2019-07-16 16:58:02 +0700

    - delete useless file

--- Pokemon/Resources/res/Map/0.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="0" tilewidth="32" tileheight="32" tilecount="24" columns="3">
- <image source="../../../../../../../Downloads/asu/0.png" width="100" height="285"/>
-</tileset>
