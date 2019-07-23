04e0092e900e3aef2a1205e33b444b08c3c3751d
ngokhuongduy <ngokhuongduy39@gmail.com>
2019-07-23 13:39:11 +0700

    * Rewrite Map

---
+++ Pokemon/Resources/res/Map/d2twtha-9929eac5-3918-4922-9a55-d49508047cc0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="d2twtha-9929eac5-3918-4922-9a55-d49508047cc0" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="d2twtha-9929eac5-3918-4922-9a55-d49508047cc0.png" width="1024" height="1024"/>
+</tileset>
